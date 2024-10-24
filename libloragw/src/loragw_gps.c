48f0ea613523730bb6ca963c1c1cbaf6456e2354
Xose Pérez <xose.perez@gmail.com>
2024-10-24 14:32:54 +0200

    Include changes required to support gpsd

--- libloragw/src/loragw_gps.c
+++ libloragw/src/loragw_gps.c
@@ -535,12 +535,19 @@ enum gps_msg lgw_parse_nmea(const char *serial_buff, int buff_size) {
         memcpy(parser_buf, serial_buff, buff_size);
         parser_buf[buff_size] = '\0';
         nb_fields = str_chop(parser_buf, buff_size, ',', str_index, ARRAY_SIZE(str_index));
-        if (nb_fields != 13) {
+        // Depending the implementation, there are 12 or 13 fields.
+        // If using the uBlox NMEA output directly there are 13 fields, if using
+        // Gpsd, there are 12 fields.
+        if (nb_fields < 12) {
             DEBUG_MSG("Warning: invalid RMC sentence (number of fields)\n");
             return IGNORED;
         }
         /* parse GPS status */
+        if (nb_fields < 13) {
+            gps_mod = *(parser_buf + str_index[2]); /* get first character, no need to bother with sscanf */
+        } else {
         gps_mod = *(parser_buf + str_index[12]); /* get first character, no need to bother with sscanf */
+        }
         if ((gps_mod != 'N') && (gps_mod != 'A') && (gps_mod != 'D')) {
             gps_mod = 'N';
         }
